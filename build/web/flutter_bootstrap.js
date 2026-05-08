b8b742a731d47e7db58e276de38d4875b8cdc7c2
Mohitha1514 <mohithapapudesi14@gmail.com>
2026-05-08 09:28:43 +0530

    Restore Flutter web app

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3090524091" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "929698645" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
